f0c13af97913b171bbb8d37e62eaf8fff82acbd8
serkansahinz <77185215+serkansahinz@users.noreply.github.com>
2023-06-24 20:18:10 +0300

    24.06.2023

--- src/AraDönemOdevi/Personel.java
+++ src/AraDönemOdevi/Personel.java
@@ -9,4 +9,40 @@ public class Personel {
     private LocalDate startDate;
     private Departman departman;
     private int baseLeave;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getiD() {
+		return iD;
+	}
+	public void setiD(int iD) {
+		this.iD = iD;
+	}
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	public Departman getDepartman() {
+		return departman;
+	}
+	public void setDepartman(Departman departman) {
+		this.departman = departman;
+	}
+	public int getBaseLeave() {
+		return baseLeave;
+	}
+	public void setBaseLeave(int baseLeave) {
+		this.baseLeave = baseLeave;
+	}
 }
